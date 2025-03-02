2a0e3b1ff8a89e41429653d787df9d08f9812c82
korokanta99 <korokanta99@gmail.com>
2025-03-02 14:41:26 +0800

    SignUp_Page

--- MauiApp1/MauiApp1/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/MauiApp1/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0a24782ea2232a305d0b4e51e9efe1413988a0d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
